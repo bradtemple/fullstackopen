7fc9dda9fff9c54a614d98a77805b61080c06018
Brad Temple <brad.temple@gmail.com>
2020-05-14 19:28:50 -0400

    uploaded part 2a

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Header = ({ course }) => {
+  return (
+    <h2>{course.name}</h2>
+  )
+}
+
+const Total = ({ course }) => {
+  let sum = course.parts.reduce((total, part) => total + part.exercises, 0)
+  return (
+    <strong>Number of exercises {sum}</strong>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(part => 
+        <Part part={part} />
+      )}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+export default Course
